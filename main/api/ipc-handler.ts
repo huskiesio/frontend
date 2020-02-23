09521a656b7847071fb632a69d6fe7a3e92e78b6
Trevor Sears <trevorsears.main@gmail.com>
2020-02-23 04:02:32 -0500

    Added more IPC functions.

--- main/api/ipc-handler.ts
+++ main/api/ipc-handler.ts
@@ -6,18 +6,52 @@
 
 import * as filesystem from "fs";
 import { ipcMain, IpcMainInvokeEvent, WebContents } from "electron";
-import { HuskyChatBot } from "@huskiesio/bot";
-import { IHCBotMessage as Message, IHCBotUser } from "@huskiesio/bot/dts/types";
+import { HCBotInfoMe, HuskyChatBot } from "@huskiesio/bot";
+import { IHCBotThread, IHCBotUser } from "@huskiesio/bot/dts/types";
+import { HCBotChatOnReceivedParam } from "@huskiesio/bot/dts/chat/HCBotChat";
+
+export type SignUpFields = {
+	
+	email: string,
+	password: string,
+	firstName: string,
+	lastName: string,
+	deviceName: string
+	
+};
+
+export type UserInfo = {
+	
+	id: string,
+	username: string,
+	firstName: string,
+	lastName: string,
+	avatar: Buffer
+	
+};
+
+export type ThreadUpdateNotification = {
+	
+	threadID: string,
+	name: string,
+	tagline: string
+	
+};
 
 export function registerHandlers(webContents: WebContents): void {
 
 	let bot: HuskyChatBot = undefined as unknown as HuskyChatBot;
-
-	ipcMain.handle("send-message", async (event: IpcMainInvokeEvent, args: { threadID: string, message: string }): Promise<string> => {
-
+	
+	ipcMain.handle("send-message", async (event: IpcMainInvokeEvent, args: { threadID: string, message: string }): Promise<Message> => {
+		
 		if (bot === undefined) throw getUninitializedBotError("send-message");
-		else return bot.chat().send(args.threadID, args.message);
-
+		else {
+			
+			let threadID: string = await bot.chat().send(args.threadID, args.message);
+			return undefined as unknown as Message;
+			
+		}
+		
 	});
 
 	ipcMain.handle("create-conversation", async (event: IpcMainInvokeEvent, args: {  }): Promise<any> => {
@@ -40,33 +74,75 @@ export function registerHandlers(webContents: WebContents): void {
 		else bot.info().me().avatar().set(filesystem.readFileSync(args.filePath));
 
 	});
-
-	ipcMain.handle("set-group-info", async (event: IpcMainInvokeEvent, args: { name?: string, tagline?: string }): Promise<void> => {
-
+	
+	ipcMain.handle("set-group-info", async (event: IpcMainInvokeEvent, args: { threadID: string, name: string, tagline: string }): Promise<void> => {
+		
 		if (bot === undefined) throw getUninitializedBotError("set-group-info");
-		// else TODO [2/22/20 @ 8:27 PM] - Finish the 'set-user-avatar' method.
-
+		// else TODO [2/22/20 @ 8:27 PM] - Finish the 'set-group-info' method.
+		
 	});
-
-	ipcMain.handle("sign-up", async (event: IpcMainInvokeEvent, args: { }): Promise<void> => {
-
-		// TODO [2/22/20 @ 8:51 PM] - Finish the 'sign-up' method.
-
+	
+	ipcMain.handle("sign-up-start", async (event: IpcMainInvokeEvent, args: SignUpFields): Promise<string> => {
+		
+		return HuskyChatBot.signUpStart(args);
+		
 	});
-
+	
+	ipcMain.handle("sign-up-finish", async (event: IpcMainInvokeEvent, args: { code: string, token: string }): Promise<string> => {
+		
+		return HuskyChatBot.signUpFinish(args.code, args.token);
+		
+	});
+	
 	ipcMain.handle("sign-in", async (event: IpcMainInvokeEvent, args: { username: string, password: string }): Promise<void> => {
-
-		bot = await HuskyChatBot.signIn(args.username, args.password);
-
+		
+		bot = await HuskyChatBot.signIn(args.username, args.password, undefined as any); // TODO [2/23/20 @ 3:42 AM] - Change me!
+		
 	});
-
-	bot.chat().onReceived(async (message: Message): Promise<void> => {
-
+	
+	ipcMain.handle("is-signed-in", async (event: IpcMainInvokeEvent, args: void): Promise<boolean> => {
+		
+		return (bot !== undefined);
+		
+	});
+	
+	ipcMain.handle("get-user-info", async (event: IpcMainInvokeEvent, args: void): Promise<UserInfo> => {
+		
+		let me: HCBotInfoMe = bot.info().me();
+		
+		return {
+			
+			id: await me.userId(),
+			username: await me.username(),
+			firstName: await me.firstName(),
+			lastName: await me.lastName(),
+			avatar: await me.avatar().get()
+			
+		};
+		
+	});
+	
+	bot.chat().onReceived(async (message: HCBotChatOnReceivedParam): Promise<void> => {
+		
 		webContents.send("new-message", message);
 
 	});
-
-	// TODO [2/22/20 @ 8:41 PM] - Add 'group-info-changed' notifier.
+	
+	bot.chat().onThreadUpdated(async(threadID: string): Promise<void> => {
+		
+		let thread: IHCBotThread = await bot.chat().getThreadForId(threadID);
+		
+		let update: ThreadUpdateNotification = {
+			
+			threadID,
+			name: thread.name(),
+			tagline: thread.description()
+			
+		};
+		
+		webContents.send("group-info-changed", update);
+		
+	});
 
 }
 
