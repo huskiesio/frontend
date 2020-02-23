088c3cc86a8005f47bd90bd47a4de466f5862404
Max Isom <code@maxisom.me>
2020-02-23 02:17:39 -0500

    Temp add message to local store

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,6 +5,7 @@ import UsersSidebar from "../components/users-sidebar";
 import Conversation from "../components/conversation";
 import {sendMessage} from "../utils/messages";
 import { IHCBotMessage } from "@huskiesio/bot/dts/types";
+import {getUserById} from '../utils/user';
 import "./styles/home.scss";
 
 const Home: React.ComponentType = (): React.ReactElement<{}> => {
@@ -13,14 +14,18 @@ const Home: React.ComponentType = (): React.ReactElement<{}> => {
 
   const thisThread = threads.find(t => t.id === currentThread);
 
-  const handleNewMessage = async (msg: IHCBotMessage) => {
-	const res = await sendMessage(msg);
+  const handleNewMessage = async (msg: any) => {
+    // For testing. Should be stripped out and replaced with res.
+    msg.sender = () => getUserById('1');
+    msg.payload = () => msg.message
+    msg.timestamp = () => new Date().getTime()
+	// const res = await sendMessage(msg);
+  //
+	// console.log(res);
 
-	console.log(res);
+	thisThread.messages.push(msg);
 
-	// thisThread.messages.push(msg);
-	//
-	// setThreads([...threads.filter(t => t.id !== currentThread), thisThread])
+	setThreads([...threads.filter(t => t.id !== currentThread), thisThread])
   };
 
   return (
